47fc5b2125b031871a4fac48033456dae19d275b
Supakorn <supakorn0728@gmail.com>
2024-11-26 16:04:04 +0700

    :tada: change project

---
+++ cinema.py
@@ -0,0 +1,12 @@
+class Cinema:
+    def __init__(self):
+        pass
+        
+    def add_movie(self, movie):
+        pass
+
+    def get_movies(self):
+        pass
+    
+    def buy_ticket(self):
+        pass
---
+++ movie.py
@@ -0,0 +1,10 @@
+class Movie:
+    def __init__(self):
+        pass
+    
+    def get_info(self):
+        pass
+
+    def get_seat(self):
+        pass
+    
